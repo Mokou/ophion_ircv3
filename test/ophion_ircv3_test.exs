3d2c7c5e6bb2f0becfd7f617ec47ac78c25e43b1
Ariadne Conill <ariadne@dereferenced.org>
2020-06-27 13:54:31 -0600

    add test for Ophion.IRCv3.compose/1

--- test/ophion_ircv3_test.exs
+++ test/ophion_ircv3_test.exs
@@ -1,3 +1,21 @@
 defmodule Ophion.IRCv3.Test do
   use ExUnit.Case
+
+  alias Ophion.IRCv3.Message
+
+  describe "Ophion.IRCv3.compose/1 -" do
+    test "it composes messages" do
+      message = %Message{
+        tags: %{},
+        source: "kaniini!~kaniini@localhost",
+        verb: "JOIN",
+        params: ["#chan"]
+      }
+
+      {:ok, ":kaniini!~kaniini@localhost JOIN #chan\r\n"} = Ophion.IRCv3.compose(message)
+    end
+  end
+
+  describe "Ophion.IRCv3.parse/1 -" do
+  end
 end
